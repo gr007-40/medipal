89cfe0441179da3ffa041d74a06154752cc877ba
Md Abdullahil Kafi <abdullahilkafi@iut-dhaka.edu>
2022-11-20 11:25:28 +0600

    dynamically add doctors

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,12 @@ import Head from 'next/head';
 const Header = () => {
     return (
         <Head>
+            <title>Medipal</title>
             <meta
                 name='viewport'
                 content='initial-scale=1, width=device-width, height=device-height'
             />
+            <title></title>
         </Head>
     );
 };
