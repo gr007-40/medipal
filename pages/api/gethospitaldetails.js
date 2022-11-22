7857a9e982a401c5cea86e64c1a0a50ce57feebb
Lamiya Tahsin Orpa <78364752+lamiya-tahsin@users.noreply.github.com>
2022-11-22 18:21:35 +0600

    added apis for hospital lists and hospital details

---
+++ pages/api/gethospitaldetails.js
@@ -0,0 +1,20 @@
+import mariadb from "mariadb/promise";
+const Sequelize = require("sequelize");
+import success from "../../components/success";
+
+const sequelize = new Sequelize("medipal", "root", "MariaDB", {
+  host: "127.0.0.1",
+  port: 3306,
+  dialect: "mariadb",
+});
+export default async function handler(req, res) {
+  try {
+    const query = await sequelize.query("select * from hospital_details");
+    res.status(200).json({ hospital_details: await query[0] });
+    //success();
+  } catch (error) {
+    // unhide to check error
+    res.status(500).json({ error: error.message });
+    // }
+  }
+}
