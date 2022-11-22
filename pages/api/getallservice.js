9d11db555e702d23c007395cccdea7f31e7fc45c
rahanuma228 <84383598+rahanuma228@users.noreply.github.com>
2022-11-23 02:35:39 +0600

    loading data from database using sequelize

---
+++ pages/api/getallservice.js
@@ -0,0 +1,29 @@
+import mariadb from "mariadb/promise";
+const Sequelize=require("sequelize");
+import success from "../../components/success";
+
+const sequelize= new Sequelize('medipal','root','hello123',{host:'127.0.0.1',port:3306,dialect:"mariadb"});
+export default async function handler(req,res){
+    //res.status(200).json({name:"sasa" });
+  try {
+   
+
+    const query = await sequelize.query(
+        "select *from allservices",);
+       // const [data] =  sequelize.execute(query,[]);
+        //sequelize.end();
+        //res.json({results:data});
+      //  console.log("USER FOUND"); // true
+        //console.log(await query); // 'My Title'
+        res.status(200).json({services:await query[0]});
+       //success();
+        
+  }
+   catch (error) {
+ 
+    // unhide to check error
+    res.status(500).json({ error: error.message });
+ // }
+}
+}
+
