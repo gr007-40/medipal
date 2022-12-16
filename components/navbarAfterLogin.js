c79a98cb61c094144d8c755b19fc845368150f19
Lamiya Tahsin Orpa <78364752+lamiya-tahsin@users.noreply.github.com>
2022-12-16 21:57:13 +0600

    added updated navbars after searchbar

---
+++ components/navbarAfterLogin.js
@@ -0,0 +1,104 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from 'next/Link';
+import SearchIcon from "@mui/icons-material/Search";
+import { grey } from '@mui/material/colors';
+
+
+
+const MEDIPAL = () => {
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                display: {xs: 'none', md: 'flex'},
+            }}
+        >
+            <Link
+                Type={'button'}
+                type='button'
+                href='/'
+            ><>
+                <Typography
+                    variant='h5'
+                    noWrap
+                    component='a'
+                    sx={{
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: '#BFD2F8',
+                    }}
+                >
+                    MEDI
+                </Typography>
+
+                <Typography
+                    variant='h5'
+                    noWrap
+                    component='a'
+                    sx={{
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: 'inherit',
+                    }}
+                >
+                    PAL
+                </Typography></>
+            </Link>
+           
+
+        </Box>
+        
+    );
+};
+
+function ResponsiveAppBar2() {
+    const pages = [
+        {url: '/', name: 'HOME'},
+        {url: '/about', name: 'ABOUT'},
+        {url: '/services', name: 'SERVICES'},
+        {url: '/hospitals', name: 'HOSPITALS'},
+        {url: '/signup', name: 'SIGNUP'},
+        {url: '/login', name: 'LOGIN'},
+    ];
+    return (
+        <AppBar
+            position='static'
+            sx={{background: '#020220', width: '100%'}}
+        >
+            <Toolbar disableGutters>
+                <MEDIPAL/>
+                <Box
+                    sx={{
+                        flexGrow: 1,
+                        ml: 'auto',
+                        mr: 'auto',
+                    }}
+                >
+                     <Link href="/search"><SearchIcon sx={{ color: grey[50] }}></SearchIcon></Link>
+                    {pages.map((page) => (
+                        <Link
+                            Type='button'
+                            type='button'
+                            variant='text'
+                            href={page.url}
+                            key={page.name}
+                            sx={{
+                                color: 'white',
+                                fontSize: '1.4rem',
+                            }}
+                        >
+                            {page.name}
+                        </Link>
+                    ))}
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default ResponsiveAppBar2;
